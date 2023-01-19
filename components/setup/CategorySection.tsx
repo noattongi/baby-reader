a718b61a682f13723173f4c60c567ec8daa7bca9
noattongi <noattongi@gmail.com>
2023-01-19 15:33:31 -0800

    added a basic layout for the setup page

--- components/setup/CategorySection.tsx
+++ components/setup/CategorySection.tsx
@@ -16,17 +16,40 @@ interface Props {
 const CategorySection: React.FC<Props> = ({category, words}) => {
   console.log(words);
   return (
-    <div>
-      <h2>{category}</h2>
-      <ul>
+    <div style={section}>
+      <h2 style={section_header}>{category}</h2>
+      <ul style={section_words}>
         {
           words?.map((word) => {
             return (<WordSelector word={word} />)
           })
         }
+        <div>
+          <input type='text' style={input_box}/>
+          <button>Add</button>
+        </div>
       </ul>
     </div>
   )
+}
+
+const section: React.CSSProperties = {
+  paddingTop: '30px',
+}
+
+const section_header: React.CSSProperties = {
+  fontWeight: 'bold',
+}
+
+const section_words: React.CSSProperties = {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr'
+}
+
+const input_box: React.CSSProperties = {
+  borderStyle: 'solid',
+  borderColor: 'black',
+  borderWidth: '1px',
 }
 
 export default CategorySection;
